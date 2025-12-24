746e5711dfead55a74ce7a6b89a4791725f48865
Peng Ge <pge1@student.ccc.edu>
2025-12-25 06:16:14 +0800

    Change default cityId from 101190401 to 101110101

--- scripts/weather/scripts/weather/tianqi.js
+++ scripts/weather/scripts/weather/tianqi.js
@@ -1,5 +1,5 @@
 const params = getParams($argument);
-const cityId = params.cityId || "101190401";
+const cityId = params.cityId || "101110101";
 const apiUrl = `http://t.weather.sojson.com/api/weather/city/${cityId}`;
 
 $httpClient.get(apiUrl, (error, response, data) => {
